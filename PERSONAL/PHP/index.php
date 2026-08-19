c0aa9979309de55661c9bc367f50b266518b6edb
ditopsen <jhonatanunistudi@gmail.com>
2026-08-19 10:17:08 -0500

    55

--- PERSONAL/PHP/index.php
+++ PERSONAL/PHP/index.php
@@ -1,3 +1,20 @@
-<?php
-echo "Hello world!!";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HOLA</title>
+</head>
+<body>
+    <?php
+    echo "<br>";
+    echo "<br>";
+    for ($i = 1; $i <= 5; $i++) {
+        echo "Numero: ". $i ."<br>";
+    }
+    echo "<br>";
+    ?>
+</body>
+</html>
+
+
