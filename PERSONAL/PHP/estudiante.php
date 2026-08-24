5f42bb58b581f266a5c90603977f0b1aa4363d94
ditopsen <jhonatanunistudi@gmail.com>
2026-08-24 08:02:28 -0500

    23

---
+++ PERSONAL/PHP/estudiante.php
@@ -0,0 +1,13 @@
+<?php
+class Estudiante {
+    public $id;
+    public $nombre;
+    public $apellido;
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+    }
+    public function getNombre () {
+        return $this->nombre;
+    }
+}
+?>
--- PERSONAL/PHP/index.php
+++ PERSONAL/PHP/index.php
@@ -7,12 +7,16 @@
 </head>
 <body>
     <?php
-    echo "<br>";
-    echo "<br>";
-    for ($i = 1; $i <= 5; $i++) {
-        echo "Numero: ". $i ."<br>";
-    }
-    echo "<br>";
+    include("estudiante.php");
+    $jhonatan = new Estudiante();
+    $jhonatan->setNombre("Jhonatan");
+    echo $jhonatan->getNombre();
+    // echo "<br>";
+    // echo "<br>";
+    // for ($i = 1; $i <= 5; $i++) {
+    //     echo "Numero: ". $i ."<br>";
+    // }
+    // echo "<br>";
     ?>
 </body>
 </html>
