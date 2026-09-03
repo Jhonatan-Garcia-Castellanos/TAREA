71787f02ba8c54495b70c739bc29139f626bac76
Sebastian Garcia <jsgarciam3456@gmail.com>
2026-09-03 15:58:02 -0500

    Revert "Update conexion.php"
    This reverts commit 665a9cafdd2fecd76ed65e91be2642de931705f7.

--- BACKPHP/config/conexion.php
+++ BACKPHP/config/conexion.php
@@ -2,21 +2,19 @@
 // config/conexion.php
 class Conexion {
     private $host = "localhost";
-    private $dbname = "projecto_acido";
+    private $dbname = "login_db";
     private $user = "root";
     private $password = "";
     public $conn;
 
     public function __construct() {
         try {
-            // Se añade charset=utf8mb4 para el correcto manejo de texto
-            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname;charset=utf8mb4", $this->user, $this->password);
-            
-            // Corrección: PDO en mayúsculas (PDO::ERRMODE_EXCEPTION)
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDo::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
-            die("Error en la conexión: " . $e->getMessage());
+            die("Error en la conexion: " . $e->getMessage());
         }
+
     }
 }
 ?>
