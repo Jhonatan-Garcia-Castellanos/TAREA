a74b91e03c40c67397073c56821816173197ca37
avendanoasctrl <rodriguezavendanoa7@gmail.com>
2026-09-08 07:34:57 -0500

    update DB.sql
    ENANO

--- BACKPHP/DB.sql
+++ BACKPHP/DB.sql
@@ -494,7 +494,7 @@ CREATE TRIGGER trg_auditar_cambio_rol BEFORE UPDATE ON usuario FOR EACH ROW BEGI
     END IF;
 END$$
 
-CREATE TRIGGER trg_limpiar_sesiones AFTER UPDATE ON control_accesos FOR EACH ROW BEGIN
+CREATE TRIGGER trg_limpiar_sesiones BEFORE UPDATE ON control_accesos FOR EACH ROW BEGIN
     IF NEW.Intentos_Fallidos = 0 THEN
         SET NEW.Bloqueado_Hasta = NULL;
     END IF;
